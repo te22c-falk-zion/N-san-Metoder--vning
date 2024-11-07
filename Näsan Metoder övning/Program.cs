642cdd382bb1a8232bedc5fb6f388827653840b3
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-07 16:20:40 +0100

    first commit

---
+++ Näsan Metoder övning/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata;
+
+
+GetChoice("Knife", "scythe", "A child");
+
+
+
+Console.WriteLine("Neat huh? no? ok then...");
+Console.ReadLine();
+
+
+static int GetChoice(string choice1, string choice2, string choice3)
+{
+    
+    string userChoice = "";
+    int userNumber;
+    Console.WriteLine($"1.{choice1}");
+    Console.WriteLine($"2.{choice2}");
+    Console.WriteLine($"3.{choice3}");
+
+
+    userChoice = Console.ReadLine();
+    int.TryParse(userChoice, out userNumber);
+
+    while (!userNumber.Equals(1) && !userNumber.Equals(2) && !userNumber.Equals(3))
+    {
+        Console.WriteLine("Please enter a valid input");
+        userChoice = Console.ReadLine();
+        int.TryParse(userChoice, out userNumber);
+    }
+
+    Console.WriteLine($"You picked somehtng ({userNumber})");
+    Console.ReadLine();
+
+    return userNumber;
+}
